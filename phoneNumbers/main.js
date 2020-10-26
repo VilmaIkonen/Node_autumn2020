81bdf04772f191df78a5e61f9b7cee3d612e87ca
VilmaIkonen <vilma.r.ikonen@gmail.com>
2020-10-26 14:40:55 +0200

    phoneNumbers task finished

--- phoneNumbers/main.js
+++ phoneNumbers/main.js
@@ -1,8 +1,33 @@
 'use strict';
 
-const { getTypes, getNumbersByType = require('./telephones')}
+const {
+  getTypes,
+  getNumbersByType,
+  getAllNumbersByType,
+  getAllNumbers,
+  getName,
+} = require('./telephones');
 
 console.log(getTypes());
 
 console.log(getNumbersByType('Leila', 'Hökki', 'home'));
 console.log(getNumbersByType('Matt', 'River', 'work'));
+
+try {
+  console.log(getNumbersByType('s'));
+} catch (err) {
+  console.log('Error: ' + err.message);
+}
+
+console.log(getAllNumbersByType('work'));
+
+try {
+  console.log(getAllNumbersByType());
+} catch (err) {
+  console.log(err.message);
+}
+
+console.log(getAllNumbers()); //console logs phones as objects. JSON.stringify to print them out. Or make a for-loop
+
+console.log(getName('22334455'));
+console.log(getName('1'));
--- phoneNumbers/telephones.js
+++ phoneNumbers/telephones.js
@@ -14,8 +14,8 @@ function getTypes() {
   }
 }
 
-/* ---- get number based based on on name and type ----- */
-function getNumbersByType() {
+/* ---- get number based on on name and type ----- */
+function getNumbersByType(firstname, lastname, type) {
   if (firstname && lastname && type) {
     const numbers = [];
     for (let person of phoneRegister) {
@@ -31,7 +31,49 @@ function getNumbersByType() {
   }
 }
 
+/* ---- get all numbers based on on name and type ----- */
+
+function getAllNumbersByType(type) {
+  if (!type) throw new Error('missing parameter');
+
+  const nameAndNumbers = [];
+  for (let person of phoneRegister) {
+    for (let phone of person.phones) {
+      if (phone.type === type) {
+        nameAndNumbers.push({
+          firstname: person.firstname,
+          lastname: person.lastname,
+          number: { type: phone.type, tel: phone.number },
+        });
+      }
+    }
+  }
+  return nameAndNumbers;
+}
+
+/* ---- get all numbers ----- */
+
+function getAllNumbers() {
+  return phoneRegister;
+}
+
+/* ---- get name based on number ----- */
+
+function getName(number) {
+  for (let person of phoneRegister) {
+    for (let phone of person.phones) {
+      if (phone.number === number) {
+        return { firstname: person.firstname, lastname: person.lastname };
+      }
+    }
+  }
+  return null;
+}
+
 module.exports = {
   getTypes,
   getNumbersByType,
+  getAllNumbersByType,
+  getAllNumbers,
+  getName,
 };
